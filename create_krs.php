898fb21e7409e00b7601b0107fb648d901ecca6d
Erick-Erdiansyah <erdiansyah290@gmail.com>
2024-10-10 11:42:25 +0700

    erick erdiansyah 3202216014

--- create_krs.php
+++ create_krs.php
@@ -1,13 +1,36 @@
 <?php
 // koneksi database
-
+require 'koneksi.php';
 
 // ambil data dari form
+$name = $_POST['name'];
+$nim = $_POST['nim'];
+$kelas = $_POST['kelas'];
+$makul = $_POST['makul'];
 
+$sql_mahasiswa = "INSERT INTO mahasiswa (nama, nim) VALUES ('$name', '$nim')";
 
-// kueri INSERT data
+if ($conn->query($sql_mahasiswa) === TRUE) {
+    $mahasiswa_id = $conn->insert_id;
 
+    $sql_kelas = "INSERT INTO kelas (mahasiswa_id, kelas) VALUES ('$mahasiswa_id', '$kelas')";
+    if ($conn->query($sql_kelas) === TRUE) {
+        echo "Kelas berhasil ditambahkan<br>";
+    } else {
+        echo "Error: {$sql_kelas} <br> {$conn->error}<br>";
+    }
 
-// tutup koneksi
+    foreach ($makul as $m) {
+        $sql_makul = "INSERT INTO makul (mahasiswa_id, makul) VALUES ('$mahasiswa_id', '$m')";
+        if ($conn->query($sql_makul) === TRUE) {
+            echo "Mata Kuliah $m berhasil ditambahkan<br>";
+        } else {
+            echo "Error: {$sql_makul} <br> {$conn->error}<br>";
+        }
+    }
+} else {
+    echo "Error: {$sql_mahasiswa} <br> {$conn->error}<br>";
+}
 
-?>
+// Tutup koneksi
+$conn->close();
---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+require 'koneksi.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM makul WHERE mahasiswa_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->close();
+
+$sql = "DELETE FROM kelas WHERE mahasiswa_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$stmt->close();
+
+$sql = "DELETE FROM mahasiswa WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    header("Location: read.php");
+} else {
+    echo "Error: {$stmt->error}";
+}
+
+$stmt->close();
+$conn->close();
+?>
